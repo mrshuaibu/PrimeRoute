8b570d8c2a37c058ed29b18f5cd98e8a550de376
Myles Reid <myles@reidsmailbox.com>
2024-12-11 09:41:39 -0600

    map added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2VsbGZjIiwiYSI6ImNscTE5azY3eDAzeGwyaXIycTgyMnM0ZW8ifQ.0Bp9MRAu0DmdRpUI8lnDPg';
+const map = new mapboxgl.Map({
+	container: 'map', // container ID
+	style: 'mapbox://styles/mapbox/streets-v12', // style URL
+	center: [-74.5, 40], // starting position [lng, lat]
+	zoom: 9, // starting zoom
+});
